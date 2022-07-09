700b907e68009608c887baa1a0de10544ac0e8e9
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-07-09 20:14:13 -0300

    Criação de Header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="logo"></div>
+    <nav>
+      <ul>
+        <li>Menu</li>
+        <li>Sobre</li>
+        <li>Carrinho</li>
+      </ul>
+    </nav>
+    <div class="login">
+      <button>Login</button>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
