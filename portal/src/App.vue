074ee56d294146bdaf83cb75aeb6a9c611e54060
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-07-09 22:02:12 -0300

    Configuração do ESLint

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import Header from "./components/Header.vue";
+import { defineComponent } from 'vue';
+import Header from './components/Header.vue';
 
 export default defineComponent({
   components: {
