7f1c6cc86afdb5618356659653c5de3779a91984
Nicolas Endrizzi <nicolasendrizzi@gmail.com>
2023-09-24 22:14:09 -0300

    retornar o toke ao dar update

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { User } from "../models";
+import jwt from 'jsonwebtoken'
 import UserRepositoryTransaction from "../repositories/user/UserRepositoryTransaction";
 
 class UserController {
@@ -28,7 +29,13 @@ class UserController {
 
     const user = await new UserRepositoryTransaction().update(id, req.body);
 
-    return res.json(user);
+    const expirationTime = new Date(Date.now() + 1 * 60 * 60 * 1000).getTime() // 1 Hour
+
+    const token = jwt.sign(Object.assign(user, { password: null, exp: expirationTime }), (process.env.JWT_SECRET_KEY ?? '').toString());
+
+    const jsonUser = Object.assign(user, { password: null, token: token });
+
+    return res.json(jsonUser)
   }
 
   public async delete(req: Request, res: Response): Promise<void> {
